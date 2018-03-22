6a26c39b213d357746bf56d57a2bd5d2cc77e117
Ally <allyrvargas@gmail.com>
2018-03-21 18:43:01 -0700

    Album route and component added

--- src/App.js
+++ src/App.js
@@ -1,27 +1,1 @@
-import React, { Component } from 'react';
-import { Route, Link } from 'react-router-dom';
-import './App.css';
-import Landing from './components/Landing';
-import Library from './components/Library';
-
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header>
-          <nav>
-            <Link to ='/'>Landing</Link>
-            <Link to ='/library'>Library</Link>
-          </nav>
-          <h1>Bloc Jams</h1>
-        </header>
-        <main>
-          <Route exact path="/" component={Landing} />
-          <Route path="/library" component={Landing} />
-        </main>
-      </div>
-    );
-  }
-}
-
-export default App;
+import React, { Component } from 'react';import { Route, Link } from 'react-router-dom';import './App.css';import Landing from './components/Landing';import Library from './components/Library';import Album from './components/Album';class App extends Component {  render() {    return (      <div className="App">        <header>          <nav>            <Link to ='/'>Landing</Link>            <Link to ='/library'>Library</Link>          </nav>          <h1>Bloc Jams</h1>        </header>        <main>          <Route exact path="/" component={Landing} />          <Route path="/library" component={Landing} />          <Route path="/album" component={Album} />        </main>      </div>    );  }}export default App;
