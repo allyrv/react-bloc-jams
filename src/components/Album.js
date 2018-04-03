2dce34c24d25589d6ce85e7a7a5282a9112e59f3
Ally <allyrvargas@gmail.com>
2018-04-02 20:16:51 -0700

    time control slides

--- src/components/Album.js
+++ src/components/Album.js
@@ -16,6 +16,8 @@ class Album extends Component {
 	this.state = {
 		album: album,
 		currentSong: album.songs[0],
+		currentTime: 0,
+		duration: album.songs[0].duration,
 		isPlaying: false
 	};
 
@@ -32,6 +34,26 @@ pause() {
 	this.audioElement.pause();
 	this.setState({ isPlaying: false });
 }
+
+componentDidMount() {
+	this.eventListeners = {
+		timeupdate: e => {
+			this.setState({ currentTime: this.audioElement.currentTime });
+		},
+		durationchange: e => {
+			this.setState({ duration: this.audioElement.duration });
+		}
+	};
+	this.audioElement.addEventListener('timeupdate', this.eventListeners.timeupdate);
+	this.audioElement.addEventListener('durationchange', this.eventListeners.durationchange);
+}
+
+componentWillUnmount() {
+	this.audioElement.src = null;
+    this.audioElement.removeEventListener('timeupdate', this.eventListeners.timeupdate);
+    this.audioElement.removeEventListener('durationchange', this.eventListeners.durationchange);
+}
+
 
 setSong(song) {
 	this.audioElement.src = song.audioSrc;
@@ -62,6 +84,12 @@ handleNextClick() {
 	const newSong = this.state.album.songs[newIndex];
 	this.setSong(newSong);
 	this.play(newSong);
+}
+
+handleTimeChange(e) {
+	const newTime = this.audioElement.duration * e.target.value;
+	this.audioElement.currentTime = newTime;
+	this.setState({ currentTime: newTime });
 }
 
 
@@ -100,9 +128,12 @@ handleNextClick() {
 				<PlayerBar
 					isPlaying={this.state.isPlaying}
 					currentSong={this.state.currentSong}
+					currentTime={this.audioElement.currentTime}
+           			duration={this.audioElement.duration}
 					handleSongClick={() => this.handleSongClick(this.state.currentSong)}
 					handlePrevClick={() => this.handlePrevClick()}
 					handleNextClick={() => this.handleNextClick()}
+					handleTimeChange={(e) => this.handleTimeChange(e)}
          		/>        	
          	</section>		
 		);
