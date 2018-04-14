41e82dc7173cf5161b93f03cda59c19a114a5c5c
Ally <allyrvargas@gmail.com>
2018-04-13 17:56:10 -0700

    Styles added

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -5,16 +5,16 @@ const Landing = () => (
 		<h1 className="hero-title">Turn the music up!</h1>
 
 		<section className="selling-points">
-			<div className="point">
-				<h2 className="point-title">Choose your music</h2>
+			<div className="point1">
+				<h2 className="point1-title">Choose your music</h2>
 				<p className="point-description">The world is full of music; why should you have to listen to music that someone else chose?</p>
 			</div>
-			<div className="point">
-				<h2 className="point-title">Unlimited, streaming, ad-free</h2>
+			<div className="point2">
+				<h2 className="point2-title">Unlimited, streaming, ad-free</h2>
 				<p className="point-description">No arbitrary limits. No distractions.</p>
 			</div>
-			<div className="point">
-				<h2 className="point-title">Mobile enabled</h2>
+			<div className="point3">
+				<h2 className="point3-title">Mobile enabled</h2>
 				<p className="point-description">Listen to your music on the go. This streaming service is available on all mobile platforms.</p>
 			</div>
 		</section>	
